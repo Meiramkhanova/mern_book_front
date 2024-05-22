ee4ab1c38977ed63aaca8394bccbfbcb11a38e2c
Adina <adina.meiramkhanova@narxoz.kz>
2024-05-22 10:46:43 +0500

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+import CreateBook from "./components/CreateBook.jsx";
+import ShowBookList from "./components/ShowBookList";
+import ShowBookDetails from "./components/ShowBookDetails.jsx";
+import UpdateBookInfo from "./components/UpdateBookInfo.jsx";
+
+const router = createBrowserRouter([
+  { path: "/", element: <ShowBookList /> },
+  { path: "/create-book", element: <CreateBook /> },
+  { path: "/show-book/:id", element: <ShowBookDetails /> },
+  { path: "/edit-book/:id", element: <UpdateBookInfo /> },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
